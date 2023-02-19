8c0d20e250e16aaa4b0e3502f8e5add0bdc5ec5a
Kio0 <62430974+Kio0@users.noreply.github.com>
2023-02-19 09:13:32 +0300

    Add files via upload

--- backend/run_http_server.py
+++ backend/run_http_server.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
 from save_userdate import FormData
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # добавляем CORS к приложению
 form_data = FormData()
 
 @app.route('/api/save_data', methods=['POST'])
